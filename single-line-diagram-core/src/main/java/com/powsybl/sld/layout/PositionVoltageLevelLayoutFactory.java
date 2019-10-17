8480c252fb0a7db6b8d1e2a43d56d619c0900b11
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2019-10-17 15:35:32 +0200

    Cleanup
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- single-line-diagram-core/src/main/java/com/powsybl/sld/layout/PositionVoltageLevelLayoutFactory.java
+++ single-line-diagram-core/src/main/java/com/powsybl/sld/layout/PositionVoltageLevelLayoutFactory.java
@@ -33,10 +33,6 @@ public class PositionVoltageLevelLayoutFactory implements VoltageLevelLayoutFact
         this.positionFinder = Objects.requireNonNull(positionFinder);
     }
 
-    PositionFinder getPositionFinder() {
-        return positionFinder;
-    }
-
     public boolean isFeederStacked() {
         return feederStacked;
     }
