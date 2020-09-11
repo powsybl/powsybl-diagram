9e3ee58d6dbad36a14ecbcaa47b5910e8352adce
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2020-09-11 12:48:12 +0200

    Add missing author and license
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

--- single-line-diagram-core/src/main/java/com/powsybl/sld/model/BusBreakerConnection.java
+++ single-line-diagram-core/src/main/java/com/powsybl/sld/model/BusBreakerConnection.java
@@ -1,8 +1,18 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.sld.model;
 
 import static com.powsybl.sld.library.ComponentTypeName.BUSBREAKER_CONNECTION;
 
+/**
+ * @author Jacques Borsenberger <jacques.borsenberger at rte-france.com>
+ */
 public class BusBreakerConnection extends FictitiousNode {
+
     public BusBreakerConnection(Graph graph, String id) {
         super(graph, id, BUSBREAKER_CONNECTION);
     }
