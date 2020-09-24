fea8b54a772c9b665be7d52238c11dc511bfaf68
jacqbors <jacques.borsenberger@rte-france.com>
2020-09-24 12:39:05 +0200

    add missing fictitious nodes
    Signed-off-by: jacqbors <jacques.borsenberger@rte-france.com>

--- single-line-diagram-core/src/main/java/com/powsybl/sld/model/BodyPrimaryBlock.java
+++ single-line-diagram-core/src/main/java/com/powsybl/sld/model/BodyPrimaryBlock.java
@@ -74,7 +74,9 @@ public class BodyPrimaryBlock extends AbstractPrimaryBlock {
         int h = 0;
         for (Node node : nodes) {
             node.setY(getCoord().getY());
-            node.setX(x0 + xPxStep * h);
+            if (!(node instanceof BusBreakerConnection)) {
+                node.setX(x0 + xPxStep * h);
+            }
             node.setRotationAngle(90.);
             h++;
         }
