a86b64e25205b314d3cbcba36edbcc89e369405d
NOIR Nicolas ext <nicolas.noir@rte-france.com>
2021-03-23 14:32:47 +0100

    Addition of missing headers
    Signed-off-by: NOIR Nicolas ext <nicolas.noir@rte-france.com>

--- single-line-diagram-iidm-extensions/src/main/java/com/powsybl/sld/iidm/extensions/BranchStatusImpl.java
+++ single-line-diagram-iidm-extensions/src/main/java/com/powsybl/sld/iidm/extensions/BranchStatusImpl.java
@@ -1,8 +1,17 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.sld.iidm.extensions;
 
 import com.powsybl.commons.extensions.AbstractExtension;
 import com.powsybl.iidm.network.Connectable;
 
+/**
+ * @author Nicolas Noir <nicolas.noir at rte-france.com>
+ */
 public class BranchStatusImpl<C extends Connectable<C>> extends AbstractExtension<C> implements BranchStatus<C> {
 
     private Status status;
