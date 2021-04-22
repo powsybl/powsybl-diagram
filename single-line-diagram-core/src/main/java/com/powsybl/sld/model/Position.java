a9a1d83d904a544e2661deb68d56c17ff67131f8
Mathieu BAGUE <mathieu.bague@rte-france.com>
2021-04-22 10:07:08 +0200

    Fix license header (#235)
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>

--- single-line-diagram-core/src/main/java/com/powsybl/sld/model/Position.java
+++ single-line-diagram-core/src/main/java/com/powsybl/sld/model/Position.java
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2019, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
- * License, vSeg. 2.0. If a copy of the MPL was not distributed with this
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package com.powsybl.sld.model;
