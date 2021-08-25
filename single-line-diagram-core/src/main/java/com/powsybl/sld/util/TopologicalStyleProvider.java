7a06fe39777b0cb33fa81d44a166c929d16b5b7a
Florian Dupuy <florian.dupuy@rte-france.com>
2021-08-25 20:36:40 +0200

    Connected nodes set removed, two id maps: node ids / equipment ids
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- single-line-diagram-core/src/main/java/com/powsybl/sld/util/TopologicalStyleProvider.java
+++ single-line-diagram-core/src/main/java/com/powsybl/sld/util/TopologicalStyleProvider.java
@@ -25,8 +25,8 @@ import java.util.stream.Collectors;
  */
 public class TopologicalStyleProvider extends AbstractBaseVoltageDiagramStyleProvider {
 
-    private final Map<String, Map<String, String>> voltageLevelStyleMap = new HashMap<>();
-    private final Map<Node, Set<Node>> connectedNodesMap = new LinkedHashMap<>();
+    private final Map<String, Map<String, String>> vlNodeIdStyleMap = new HashMap<>();
+    private final Map<String, Map<String, String>> vlEquipmentIdStyleMap = new HashMap<>();
 
     public TopologicalStyleProvider(Network network) {
         this(BaseVoltageStyle.fromPlatformConfig(), network);
@@ -63,47 +63,44 @@ public class TopologicalStyleProvider extends AbstractBaseVoltageDiagramStylePro
 
     @Override
     public void reset() {
-        voltageLevelStyleMap.clear();
-        connectedNodesMap.clear();
+        vlNodeIdStyleMap.clear();
+        vlEquipmentIdStyleMap.clear();
     }
 
-    private Map<String, String> createStyleMap(String baseVoltageLevelStyle, VoltageLevelInfos voltageLevelInfos) {
+    private Map<String, String> createEquipmentIdStyleMap(String baseVoltageLevelStyle, VoltageLevelInfos voltageLevelInfos) {
         VoltageLevel vl = network.getVoltageLevel(voltageLevelInfos.getId());
         List<Bus> buses = vl.getBusView().getBusStream().collect(Collectors.toList());
 
-        Map<String, String> styleMap = new HashMap<>();
+        Map<String, String> equipmentIdStyleMap = new HashMap<>();
         AtomicInteger idxStyle = new AtomicInteger(0);
         buses.forEach(b -> {
             String style = baseVoltageLevelStyle + '-' + idxStyle.getAndIncrement();
-            b.visitConnectedEquipments(new TopologyVisitorId(equipmentId -> styleMap.put(equipmentId, style)));
+            b.visitConnectedEquipments(new TopologyVisitorId(equipmentId -> equipmentIdStyleMap.put(equipmentId, style)));
         });
 
-        return styleMap;
+        return equipmentIdStyleMap;
     }
 
     private Optional<String> getNodeTopologicalStyle(String baseVoltageLevelStyle, VoltageLevelInfos
             voltageLevelInfos, Node node) {
-        Map<String, String> styleMap = getVoltageLevelStyleMap(baseVoltageLevelStyle, voltageLevelInfos);
-        String nodeTopologicalStyle = styleMap.computeIfAbsent(
-            node.getEquipmentId(), id -> findConnectedStyle(styleMap, node).orElse(null));
+        Map<String, String> equipmentIdStyleMap = getEquipmentIdStyleMap(baseVoltageLevelStyle, voltageLevelInfos);
+        Map<String, String> nodeIdStyleMap = vlNodeIdStyleMap.computeIfAbsent(voltageLevelInfos.getId(), k -> new HashMap<>());
+        String nodeTopologicalStyle = nodeIdStyleMap.getOrDefault(node.getId(), findConnectedStyle(equipmentIdStyleMap, nodeIdStyleMap, node));
         return Optional.ofNullable(nodeTopologicalStyle);
     }
 
-    private Optional<String> findConnectedStyle(Map<String, String> styleMap, Node node) {
-        Set<Node> connectedNodes = connectedNodesMap.getOrDefault(node, findConnectedNodes(node));
-        return connectedNodes.stream().map(c -> styleMap.get(c.getEquipmentId())).filter(Objects::nonNull).findFirst();
+    private String findConnectedStyle(Map<String, String> equipmentIdStyleMap, Map<String, String> nodeIdStyleMap, Node node) {
+        Set<Node> connectedNodes = new HashSet<>();
+        findConnectedNodes(node, connectedNodes);
+        String connectedStyle = connectedNodes.stream().map(c -> equipmentIdStyleMap.get(c.getEquipmentId())).filter(Objects::nonNull).findFirst().orElse(null);
+        connectedNodes.forEach(n -> nodeIdStyleMap.put(n.getId(), connectedStyle));
+        return connectedStyle;
     }
 
-    private Map<String, String> getVoltageLevelStyleMap(String baseVoltageLevelStyle, VoltageLevelInfos
+    private Map<String, String> getEquipmentIdStyleMap(String baseVoltageLevelStyle, VoltageLevelInfos
             voltageLevelInfos) {
-        return voltageLevelStyleMap.computeIfAbsent(
-                voltageLevelInfos.getId(), k -> createStyleMap(baseVoltageLevelStyle, voltageLevelInfos));
-    }
-
-    private Set<Node> findConnectedNodes(Node node) {
-        Set<Node> visitedNodes = new HashSet<>();
-        findConnectedNodes(node, visitedNodes);
-        return visitedNodes;
+        return vlEquipmentIdStyleMap.computeIfAbsent(
+                voltageLevelInfos.getId(), k -> createEquipmentIdStyleMap(baseVoltageLevelStyle, voltageLevelInfos));
     }
 
     private void findConnectedNodes(Node node, Set<Node> visitedNodes) {
@@ -114,7 +111,6 @@ public class TopologicalStyleProvider extends AbstractBaseVoltageDiagramStylePro
             return;
         }
         visitedNodes.add(node);
-        connectedNodesMap.put(node, visitedNodes);
         for (Node adjNode : node.getAdjacentNodes()) {
             findConnectedNodes(adjNode, visitedNodes);
         }
