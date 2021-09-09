0a9105cde64f7194865ddba8457ec202cc605fa0
Florian Dupuy <66690739+flo-dup@users.noreply.github.com>
2021-09-09 18:01:46 +0200

    Remove useless rotations (#259)
    * Remove node/bus connection rotations
    * Avoid matrix transformation if rotation=0
    * Label rotation only if angle>0
    * Update unit tests
    
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- single-line-diagram-core/src/main/java/com/powsybl/sld/svg/DefaultSVGWriter.java
+++ single-line-diagram-core/src/main/java/com/powsybl/sld/svg/DefaultSVGWriter.java
@@ -657,7 +657,9 @@ public class DefaultSVGWriter implements SVGWriter {
         Element label = g.getOwnerDocument().createElement("text");
         label.setAttribute("x", String.valueOf(xShift));
         label.setAttribute("y", String.valueOf(yShift));
-        label.setAttribute(TRANSFORM, ROTATE + "(" + shiftAngle + "," + 0 + "," + 0 + ")");
+        if (shiftAngle != 0) {
+            label.setAttribute(TRANSFORM, ROTATE + "(" + shiftAngle + "," + 0 + "," + 0 + ")");
+        }
         label.setAttribute(CLASS, LABEL_STYLE_CLASS);
         Text text = g.getOwnerDocument().createTextNode(str);
         label.appendChild(text);
@@ -872,17 +874,23 @@ public class DefaultSVGWriter implements SVGWriter {
             if (arrowRotationAngle < -Math.PI / 2) {
                 arrowRotationAngle += Math.PI;
             }
-            g.setAttribute(TRANSFORM, getTransformMatrixString(x, y, arrowRotationAngle, componentSize));
+            g.setAttribute(TRANSFORM, getTransformString(x, y, arrowRotationAngle, componentSize));
         }
 
     }
 
-    private String getTransformMatrixString(double centerPosX, double centerPosY, double angle, ComponentSize componentSize) {
+    private String getTransformString(double centerPosX, double centerPosY, double angle, ComponentSize componentSize) {
         double centerPosTransX = layoutParameters.getTranslateX() + centerPosX;
         double centerPosTransY = layoutParameters.getTranslateY() + centerPosY;
-        double[] matrix = getTransformMatrix(componentSize.getWidth(), componentSize.getHeight(), angle,
+        if (angle == 0) {
+            double translateX = centerPosTransX - componentSize.getWidth() / 2;
+            double translateY = centerPosTransY - componentSize.getHeight() / 2;
+            return TRANSLATE + "(" +  translateX + "," + translateY + ")";
+        } else {
+            double[] matrix = getTransformMatrix(componentSize.getWidth(), componentSize.getHeight(), angle,
                 centerPosTransX, centerPosTransY);
-        return transformMatrixToString(matrix, 4);
+            return transformMatrixToString(matrix, 4);
+        }
     }
 
     private double[] getTransformMatrix(double width, double height, double angle,
