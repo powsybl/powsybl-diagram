05238a0e0ad5e690c130028894377b4c2d514629
Florian Dupuy <florian.dupuy@rte-france.com>
2021-09-10 17:03:33 +0200

    Add frame with css style default to lightgrey
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- single-line-diagram-core/src/main/java/com/powsybl/sld/svg/DiagramStyles.java
+++ single-line-diagram-core/src/main/java/com/powsybl/sld/svg/DiagramStyles.java
@@ -37,6 +37,7 @@ public final class DiagramStyles {
     public static final String BOTTOM_FEEDER = STYLE_PREFIX + "bottom-feeder";
     public static final String TOP_FEEDER = STYLE_PREFIX + "top-feeder";
     public static final String CONSTANT_COLOR_CLASS = STYLE_PREFIX + "constant-color";
+    public static final String FRAME_CLASS = STYLE_PREFIX + "frame";
 
     private static final String ID_PREFIX = "id";
 
