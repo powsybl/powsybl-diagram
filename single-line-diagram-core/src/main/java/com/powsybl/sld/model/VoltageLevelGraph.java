0de3691543aa86bbfb64a5c1e63a80f442c8872b
BenoitJeanson <benoit.jeanson@rte-france.com>
2022-02-16 10:52:39 +0100

    fix BusConnection was not added in vlGraph
    Signed-off-by: BenoitJeanson <benoit.jeanson@rte-france.com>

--- single-line-diagram-core/src/main/java/com/powsybl/sld/model/VoltageLevelGraph.java
+++ single-line-diagram-core/src/main/java/com/powsybl/sld/model/VoltageLevelGraph.java
@@ -423,9 +423,11 @@ public class VoltageLevelGraph extends AbstractBaseGraph {
                         removeEdge(n1, n2);
                         String busToBusId = n1.getId() + "-" + n2.getId();
                         BusConnection fSwToBus1 = new BusConnection(busToBusId + "_1", this);
+                        addNode(fSwToBus1);
                         InternalNode internalNode1 = new InternalNode(busToBusId + "_1", this);
                         InternalNode internalNode2 = new InternalNode(busToBusId + "_2", this);
                         BusConnection fSwToBus2 = new BusConnection(busToBusId + "_2", this);
+                        addNode(fSwToBus1);
                         addEdge(n1, fSwToBus1);
                         addEdge(fSwToBus1, internalNode1);
                         addEdge(internalNode1, internalNode2);
