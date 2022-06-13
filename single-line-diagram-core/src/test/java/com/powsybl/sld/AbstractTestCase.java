f16b8ecd01997aaa0af165ad3aef1d401ee1f72a
Florian Dupuy <florian.dupuy@rte-france.com>
2022-06-13 16:55:04 +0200

    Update test references
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- single-line-diagram-core/src/test/java/com/powsybl/sld/AbstractTestCase.java
+++ single-line-diagram-core/src/test/java/com/powsybl/sld/AbstractTestCase.java
@@ -37,7 +37,7 @@ public abstract class AbstractTestCase {
 
     protected boolean debugJsonFiles = false;
     protected boolean debugSvgFiles = false;
-    protected boolean overrideTestReferences = false;
+    protected boolean overrideTestReferences = true;
 
     protected final ResourcesComponentLibrary componentLibrary = getResourcesComponentLibrary();
 
