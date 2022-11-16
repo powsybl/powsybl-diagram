49a7ab3175ca8a25cd23bd4ba165e90e1ebabd7e
Florian Dupuy <florian.dupuy@rte-france.com>
2022-11-16 10:39:13 +0100

    Fix version classes
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- diagram-util/src/main/java-templates/com/powsybl/diagram/util/PowsyblDiagramVersion.java
+++ diagram-util/src/main/java-templates/com/powsybl/diagram/util/PowsyblDiagramVersion.java
@@ -1,10 +1,10 @@
 /**
- * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package com.powsybl.nad.util;
+package com.powsybl.diagram.util;
 
 import com.google.auto.service.AutoService;
 import com.powsybl.tools.*;
@@ -13,9 +13,9 @@ import com.powsybl.tools.*;
  * @author Florian Dupuy <florian.dupuy at rte-france.com>
  */
 @AutoService(Version.class)
-public class PowsyblNetworkAreaDiagramVersion extends AbstractVersion {
+public class PowsyblDiagramVersion extends AbstractVersion {
 
-    public PowsyblNetworkAreaDiagramVersion() {
-        super("powsybl-network-area-diagram", "${project.version}", "${buildNumber}", "${scmBranch}", Long.parseLong("${timestamp}"));
+    public PowsyblDiagramVersion() {
+        super("powsybl-diagram", "${project.version}", "${buildNumber}", "${scmBranch}", Long.parseLong("${timestamp}"));
     }
 }
