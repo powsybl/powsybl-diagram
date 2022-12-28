38ad1dd211512ce72b688734e62bf69f8b0fa04a
Luma <zamarrenolm@aia.es>
2022-12-28 17:07:12 +0100

    add glued class to half edge drawings
    Signed-off-by: Luma <zamarrenolm@aia.es>

--- network-area-diagram/src/main/java/com/powsybl/nad/svg/SvgWriter.java
+++ network-area-diagram/src/main/java/com/powsybl/nad/svg/SvgWriter.java
@@ -209,7 +209,10 @@ public class SvgWriter {
         if (edge.isVisible(side)) {
             if (!graph.isLoop(edge)) {
                 writer.writeEmptyElement(POLYLINE_ELEMENT_NAME);
-                writer.writeAttribute(CLASS_ATTRIBUTE, String.join(" ", StyleProvider.EDGE_PATH_CLASS, StyleProvider.STRETCHABLE_CLASS));
+                writer.writeAttribute(CLASS_ATTRIBUTE, String.join(" ",
+                        StyleProvider.EDGE_PATH_CLASS,
+                        StyleProvider.STRETCHABLE_CLASS,
+                        StyleProvider.GLUED_CLASS + "-" + side.getNum()));
                 writer.writeAttribute(POINTS_ATTRIBUTE, getPolylinePointsString(edge, side));
                 drawBranchEdgeInfo(graph, writer, edge, side, labelProvider.getEdgeInfos(graph, edge, side));
             } else {
