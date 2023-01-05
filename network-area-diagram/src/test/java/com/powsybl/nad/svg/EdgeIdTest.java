412ee0e3f499d23e8ab58b626d545fc0fc5c6858
Sophie Frasnedo <sophie.frasnedo@rte-france.com>
2023-01-05 16:41:52 +0100

    Add new tests to display line names or not according to user choice
    Signed-off-by: Sophie Frasnedo <sophie.frasnedo@rte-france.com>

---
+++ network-area-diagram/src/test/java/com/powsybl/nad/svg/EdgeIdTest.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.nad.svg;
+
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.test.ThreeWindingsTransformerNetworkFactory;
+import com.powsybl.nad.AbstractTest;
+import com.powsybl.nad.layout.LayoutParameters;
+import com.powsybl.nad.model.BranchEdge;
+import com.powsybl.nad.model.Graph;
+import com.powsybl.nad.model.ThreeWtEdge;
+import com.powsybl.nad.svg.iidm.DefaultLabelProvider;
+import com.powsybl.nad.svg.iidm.NominalVoltageStyleProvider;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Florian Dupuy <florian.dupuy at rte-france.com>
+ */
+public class EdgeIdTest extends AbstractTest {
+
+    @Before
+    public void setup() {
+        setLayoutParameters(new LayoutParameters());
+        setSvgParameters(new SvgParameters()
+                .setSvgWidthAndHeightAdded(true)
+                .setFixedWidth(800));
+    }
+
+    @Override
+    protected StyleProvider getStyleProvider(Network network) {
+        return new NominalVoltageStyleProvider(network);
+    }
+
+    @Override
+    protected LabelProvider getLabelProvider(Network network) {
+        return new DefaultLabelProvider(network, getSvgParameters()) {
+        };
+    }
+
+    @Test
+    public void testNameOnLineDisplayed() {
+        Network network = NetworkTestFactory.createThreeVoltageLevelsFiveBuses();
+        getSvgParameters().setLineNameDisplayed(true);
+        assertEquals(toString("/edge_with_id.svg"), generateSvgString(network, "/edge_with_id.svg"));
+    }
+
+    @Test
+    public void testNameOnLineNotDisplayed() {
+        Network network = NetworkTestFactory.createThreeVoltageLevelsFiveBuses();
+        getSvgParameters().setLineNameDisplayed(false);
+        assertEquals(toString("/edge_without_id.svg"), generateSvgString(network, "/edge_without_id.svg"));
+    }
+}
