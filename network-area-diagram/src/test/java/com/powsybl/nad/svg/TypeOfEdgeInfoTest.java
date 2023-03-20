9916cea076f770bba592ca3656da1d73d7a2c2a3
Sophie Frasnedo <sophie.frasnedo@rte-france.com>
2023-03-20 15:17:51 +0100

    add tests for current and reactive power edge info (that are not displayed by default)
    Signed-off-by: Sophie Frasnedo <sophie.frasnedo@rte-france.com>

---
+++ network-area-diagram/src/test/java/com/powsybl/nad/svg/TypeOfEdgeInfoTest.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.nad.svg;
+
+import com.powsybl.ieeecdf.converter.IeeeCdfNetworkFactory;
+import com.powsybl.iidm.network.Branch;
+import com.powsybl.iidm.network.Connectable;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.Terminal;
+import com.powsybl.iidm.network.test.FourSubstationsNodeBreakerFactory;
+import com.powsybl.iidm.xml.NetworkXml;
+import com.powsybl.loadflow.LoadFlow;
+import com.powsybl.nad.AbstractTest;
+import com.powsybl.nad.layout.LayoutParameters;
+import com.powsybl.nad.svg.iidm.DefaultLabelProvider;
+import com.powsybl.nad.svg.iidm.NominalVoltageStyleProvider;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Florian Dupuy <florian.dupuy at rte-france.com>
+ */
+public class TypeOfEdgeInfoTest extends AbstractTest {
+
+    @Before
+    public void setup() {
+        setLayoutParameters(new LayoutParameters());
+        setSvgParameters(new SvgParameters()
+                .setInsertNameDesc(true)
+                .setSvgWidthAndHeightAdded(true)
+                .setVoltageLevelDetails(false)
+                .setFixedWidth(800)
+                .setEdgeStartShift(2));
+    }
+
+    @Override
+    protected StyleProvider getStyleProvider(Network network) {
+        return new NominalVoltageStyleProvider(network);
+    }
+
+    @Override
+    protected LabelProvider getLabelProvider(Network network) {
+        return new DefaultLabelProvider(network, getSvgParameters());
+    }
+
+    @Test
+    public void testReactivePowerInfoLabel() {
+        Network network = IeeeCdfNetworkFactory.create9();
+        LoadFlow.run(network);
+        getSvgParameters().setEdgeInfoDisplayed(SvgParameters.EdgeInfoEnum.REACTIVE_POWER);
+        assertEquals(toString("/edge_info_reactive_power.svg"), generateSvgString(network, "/edge_info_reactive_power.svg"));
+    }
+
+    @Test
+    public void testCurrentInfoLabel() {
+        Network network = IeeeCdfNetworkFactory.create9();
+        LoadFlow.run(network);
+        getSvgParameters().setEdgeInfoDisplayed(SvgParameters.EdgeInfoEnum.CURRENT);
+        assertEquals(toString("/edge_info_current.svg"), generateSvgString(network, "/edge_info_current.svg"));
+    }
+
+
+}
