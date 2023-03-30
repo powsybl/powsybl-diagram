2f17d39b1f132a3015eefddb5bdc302bb87d795d
Sophie Frasnedo <sophie.frasnedo@rte-france.com>
2023-03-30 16:47:17 +0200

    fix tests following merge with updates in main
    Signed-off-by: Sophie Frasnedo <sophie.frasnedo@rte-france.com>

--- single-line-diagram/single-line-diagram-core/src/test/java/com/powsybl/sld/library/ComponentLibraryTest.java
+++ single-line-diagram/single-line-diagram-core/src/test/java/com/powsybl/sld/library/ComponentLibraryTest.java
@@ -24,6 +24,6 @@ public class ComponentLibraryTest {
         ComponentLibrary cvg = ComponentLibrary.find("Convergence").orElse(null);
         assertNotNull(cvg);
         assertEquals("Convergence", cvg.getName());
-        assertEquals(20, cvg.getComponentsSize().size());
+        assertEquals(21, cvg.getComponentsSize().size());
     }
 }
