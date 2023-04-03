59e8ee00f45ea01b10d855b0b3082363b9648e92
Sophie Frasnedo <93923177+So-Fras@users.noreply.github.com>
2023-04-03 15:54:50 +0200

    Update TypeOfEdgeInfoTest.java
    fix year and author
    
    Signed-off-by: Sophie Frasnedo <93923177+So-Fras@users.noreply.github.com>

--- network-area-diagram/src/test/java/com/powsybl/nad/svg/TypeOfEdgeInfoTest.java
+++ network-area-diagram/src/test/java/com/powsybl/nad/svg/TypeOfEdgeInfoTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -19,7 +19,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 /**
- * @author Florian Dupuy <florian.dupuy at rte-france.com>
+ * @author Sophie Frasnedo <sophie.frasnedo at rte-france.com>
  */
 public class TypeOfEdgeInfoTest extends AbstractTest {
 
