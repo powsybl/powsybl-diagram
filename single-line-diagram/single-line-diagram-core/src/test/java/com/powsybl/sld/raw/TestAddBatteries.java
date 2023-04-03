ab8bc49f8ded1a1c13996393666e5b9012d65b50
Sophie Frasnedo <93923177+So-Fras@users.noreply.github.com>
2023-04-03 15:47:29 +0200

    Update TestAddBatteries.java
    remove public modifier
    
    Signed-off-by: Sophie Frasnedo <93923177+So-Fras@users.noreply.github.com>

--- single-line-diagram/single-line-diagram-core/src/test/java/com/powsybl/sld/raw/TestAddBatteries.java
+++ single-line-diagram/single-line-diagram-core/src/test/java/com/powsybl/sld/raw/TestAddBatteries.java
@@ -23,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * @author Sophie Frasnedo <sophie.frasnedo at rte-france.com>
  */
-public class TestAddBatteries extends AbstractTestCaseRaw {
+class TestAddBatteries extends AbstractTestCaseRaw {
 
     @BeforeEach
     public void setUp() {
