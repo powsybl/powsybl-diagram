cb6d9a3f56cb2c72abdee28787f8f534f30688c7
Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>
2023-07-11 09:45:08 +0200

    Highlight disconnected injections (#530)
    * Bump to v4.0.0-SNAPSHOT
    * Highlight disconnected injections
    * Wip
    * Wip
    * Wip
    * highlight connection status for internal 2WT
    * Refactor
    
    Signed-off-by: Geoffroy Jamgotchian <geoffroy.jamgotchian@rte-france.com>

---
+++ single-line-diagram/single-line-diagram-core/src/test/java/com/powsybl/sld/iidm/TestDisconnectedComponentsBusBreaker.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.sld.iidm;
+
+import com.powsybl.diagram.test.Networks;
+import com.powsybl.iidm.network.Branch;
+import com.powsybl.iidm.network.ThreeWindingsTransformer;
+import com.powsybl.sld.builders.NetworkGraphBuilder;
+import com.powsybl.sld.model.graphs.VoltageLevelGraph;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author Sophie Frasnedo <sophie.frasnedo at rte-france.com>
+ */
+
+class TestDisconnectedComponentsBusBreaker extends AbstractTestCaseIidm {
+
+    @BeforeEach
+    public void setUp() {
+        network = Networks.createBusBreakerNetworkWithInternalBranches("TestInternalBranchesBusBreaker", "test");
+        substation = network.getSubstation("S1");
+    }
+
+    @Test
+    void testDisconnectedComponents() {
+        network.getLoad("LD1").getTerminal().disconnect();
+        network.getGenerator("G").getTerminal().disconnect();
+        network.getLine("L12").getTerminal(Branch.Side.TWO).disconnect();
+        network.getTwoWindingsTransformer("T11").getTerminal(Branch.Side.TWO).disconnect();
+        network.getThreeWindingsTransformer("T3_12").getTerminal(ThreeWindingsTransformer.Side.THREE).disconnect();
+
+        // build graph
+        graphBuilder = new NetworkGraphBuilder(network);
+        VoltageLevelGraph g = graphBuilder.buildVoltageLevelGraph(network.getVoltageLevel("VL1").getId());
+
+        // Run layout
+        voltageLevelGraphLayout(g);
+
+        // write SVG and compare to reference
+        assertEquals(toString("/disconnectedComponentsBusBreaker.svg"),
+                toSVG(g, "/disconnectedComponentsBusBreaker.svg", getDefaultDiagramLabelProvider(), getDefaultDiagramStyleProvider()));
+    }
+}
