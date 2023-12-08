7c9ef6329672ed54deffd051710bfa0033bf4f8b
Florian Dupuy <florian.dupuy@rte-france.com>
2023-12-08 17:35:49 +0100

    Fix unit test
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- network-area-diagram/src/test/java/com/powsybl/nad/svg/DiagramMetadataTest.java
+++ network-area-diagram/src/test/java/com/powsybl/nad/svg/DiagramMetadataTest.java
@@ -93,16 +93,19 @@ class DiagramMetadataTest extends AbstractTest {
     @Test
     void testInvalid() throws XMLStreamException {
         // Referenced svg file
-        String reference = "<metadata>\n" +
-                "        <nad:nad xmlns:nad=\"http://www.powsybl.org/schema/nad-metadata/1_0\">\n" +
-                "            <nad:nodes>\n" +
-                "                <nad:edge diagramId=\"10\" equipmentId=\"TWT\"/>\n" +
-                "            </nad:nodes>\n" +
-                "            <nad:edges>\n" +
-                "                <nad:node diagramId=\"0\" equipmentId=\"S1VL1\"/>\n" +
-                "            </nad:edges>\n" +
-                "        </nad:nad>\n" +
-                "    </metadata>";
+        String reference = """
+                <svg>
+                    <metadata>
+                        <nad:nad xmlns:nad="http://www.powsybl.org/schema/nad-metadata/1_0">
+                            <nad:nodes>
+                                <nad:edge diagramId="10" equipmentId="TWT"/>
+                            </nad:nodes>
+                            <nad:edges>
+                                <nad:node diagramId="0" equipmentId="S1VL1"/>
+                            </nad:edges>
+                        </nad:nad>
+                    </metadata>
+                </svg>""";
         InputStream in = new ByteArrayInputStream(reference.getBytes(StandardCharsets.UTF_8));
         // Create Metadata from svg file
         DiagramMetadata metadata = DiagramMetadata.readXml(in);
@@ -114,13 +117,14 @@ class DiagramMetadataTest extends AbstractTest {
         // remove xml header (first line)
         actual = actual.substring(actual.indexOf(METADATA_START_TOKEN));
         // Keep only metadata from svg file
-        String expected = "<metadata>\n" +
-                "        <nad:nad xmlns:nad=\"http://www.powsybl.org/schema/nad-metadata/1_0\">" +
-                "            <nad:busNodes/>\n" +
-                "            <nad:nodes/>\n" +
-                "            <nad:edges/>\n" +
-                "        </nad:nad>" +
-                "    </metadata>";
+        String expected = """
+                <metadata>
+                    <nad:nad xmlns:nad="http://www.powsybl.org/schema/nad-metadata/1_0">
+                        <nad:busNodes/>
+                        <nad:nodes/>
+                        <nad:edges/>
+                    </nad:nad>
+                </metadata>""";
         // Checking
         assertEquals(removeWhiteSpaces(expected), removeWhiteSpaces(actual));
     }
