a6386a06cd6dcbc13e09506e3e77166052805f28
Florian Dupuy <florian.dupuy@rte-france.com>
2024-03-18 15:44:21 +0100

    Avoid division by zero if PrimaryBlock contained only one node
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- single-line-diagram/single-line-diagram-core/src/main/java/com/powsybl/sld/layout/CalculateCoordBlockVisitor.java
+++ single-line-diagram/single-line-diagram-core/src/main/java/com/powsybl/sld/layout/CalculateCoordBlockVisitor.java
@@ -11,6 +11,8 @@ import com.powsybl.sld.model.coordinate.Coord;
 import com.powsybl.sld.model.coordinate.Position;
 import com.powsybl.sld.model.nodes.Node;
 
+import java.util.List;
+
 import static com.powsybl.sld.model.coordinate.Orientation.*;
 import static com.powsybl.sld.model.coordinate.Position.Dimension.*;
 import static com.powsybl.sld.model.coordinate.Coord.Dimension.*;
@@ -33,26 +35,27 @@ public final class CalculateCoordBlockVisitor implements BlockVisitor {
 
     @Override
     public void visit(BodyPrimaryBlock block) {
+        List<Node> blockNodes = block.getNodes();
+        if (blockNodes.size() == 1) {
+            blockNodes.get(0).setCoordinates(block.getCoord().get(X), block.getCoord().get(Y));
+            return;
+        }
+
         if (block.getPosition().getOrientation().isVertical()) {
             int sign = block.getOrientation() == UP ? 1 : -1;
             double y0 = block.getCoord().get(Y) + sign * block.getCoord().getSpan(Y) / 2;
-            double yPxStep = block.getPosition().getSpan(V) == 0 ? 0
-                    : sign * block.getCoord().getSpan(Y) / (block.getNodes().size() - 1);
-            int v = 0;
-            for (Node node : block.getNodes()) {
-                node.setCoordinates(block.getCoord().get(X), y0 - yPxStep * v);
-                v++;
+            double yPxStep = sign * block.getCoord().getSpan(Y) / (blockNodes.size() - 1);
+            for (int i = 0; i < blockNodes.size(); i++) {
+                blockNodes.get(i).setCoordinates(block.getCoord().get(X), y0 - yPxStep * i);
             }
         } else {
             double x0 = block.getCoord().get(X) - block.getCoord().getSpan(X) / 2;
             if (layoutContext.isInternCell() && !layoutContext.isFlat()) {
                 x0 += layoutParameters.getCellWidth() / 2;
             }
-            double xPxStep = block.getCoord().getSpan(X) / (block.getNodes().size() - 1);
-            int h = 0;
-            for (Node node : block.getNodes()) {
-                node.setCoordinates(x0 + xPxStep * h, block.getCoord().get(Y));
-                h++;
+            double xPxStep = block.getCoord().getSpan(X) / (blockNodes.size() - 1);
+            for (int i = 0; i < blockNodes.size(); i++) {
+                blockNodes.get(i).setCoordinates(x0 + xPxStep * i, block.getCoord().get(Y));
             }
         }
     }
