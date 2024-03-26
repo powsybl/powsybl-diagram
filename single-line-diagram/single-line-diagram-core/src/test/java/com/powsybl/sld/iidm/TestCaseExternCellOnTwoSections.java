f184cab566255ee210e0f90c0a9bd19bf8520010
Sophie Frasnedo <sophie.frasnedo@rte-france.com>
2024-03-26 22:36:31 +0100

    Refactor existing test
    Signed-off-by: Sophie Frasnedo <sophie.frasnedo@rte-france.com>

--- single-line-diagram/single-line-diagram-core/src/test/java/com/powsybl/sld/iidm/TestCaseExternCellOnTwoSections.java
+++ single-line-diagram/single-line-diagram-core/src/test/java/com/powsybl/sld/iidm/TestCaseExternCellOnTwoSections.java
@@ -8,11 +8,7 @@
 package com.powsybl.sld.iidm;
 
 import com.powsybl.diagram.test.Networks;
-import com.powsybl.iidm.network.Country;
 import com.powsybl.iidm.network.Network;
-import com.powsybl.iidm.network.SwitchKind;
-import com.powsybl.iidm.network.TopologyKind;
-import com.powsybl.iidm.network.extensions.ConnectablePosition;
 import com.powsybl.sld.builders.NetworkGraphBuilder;
 import com.powsybl.sld.model.graphs.VoltageLevelGraph;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,39 +17,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * <PRE>
- *         l
- *         |
- *     ___ b ____
- *    |          |
- *    |          |
- * --d1--  x  --d2--
- *  bbs1  dc   bbs2
- * </PRE>
- *
  * @author Florian Dupuy {@literal <florian.dupuy at rte-france.com>}
  */
+
 class TestCaseExternCellOnTwoSections extends AbstractTestCaseIidm {
 
     @BeforeEach
     public void setUp() {
-        network = Network.create("test", "test");
-        graphBuilder = new NetworkGraphBuilder(network);
-        substation = Networks.createSubstation(network, "s", "s", Country.FR);
-        vl = Networks.createVoltageLevel(substation, "vl", "vl", TopologyKind.NODE_BREAKER, 225);
-        Networks.createBusBarSection(vl, "bbs1", "bbs1", 0, 1, 1);
-        Networks.createBusBarSection(vl, "bbs2", "bbs2", 1, 1, 2);
-        Networks.createLoad(vl, "l", "l", "l", 0, ConnectablePosition.Direction.TOP, 2, 10, 10);
-        Networks.createSwitch(vl, "d1", "d1", SwitchKind.DISCONNECTOR, false, false, false, 0, 3);
-        Networks.createSwitch(vl, "d2", "d2", SwitchKind.DISCONNECTOR, false, false, false, 1, 3);
-        Networks.createSwitch(vl, "dc", "dc", SwitchKind.DISCONNECTOR, false, false, false, 0, 1);
-        Networks.createSwitch(vl, "b", "b", SwitchKind.BREAKER, false, false, false, 3, 2);
     }
 
     @Test
-    void test() {
-        VoltageLevelGraph g = graphBuilder.buildVoltageLevelGraph(vl.getId());
+    void testSimpleExternCellOnTwoSections() {
+        network = Networks.createSimpleExternCellOnTwoSections();
+        VoltageLevelGraph g = new NetworkGraphBuilder(network).buildVoltageLevelGraph("vl");
         voltageLevelGraphLayout(g);
-        assertEquals(toString("/TestCaseExternCellOnTwoSections.svg"), toSVG(g, "/TestCaseExternCellOnTwoSections.svg"));
+        assertEquals(toString("/TestCaseSimpleExternCellOnTwoSections.svg"), toSVG(g, "/TestCaseSimpleExternCellOnTwoSections.svg"));
     }
 }
