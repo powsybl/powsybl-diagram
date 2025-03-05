8e1dfab26b1298d4303f19645e00c02ad96d56fe
Christian Biasuzzi <christian.biasuzzi@soft.it>
2025-03-05 18:59:22 +0100

    adds more customizations to the NAD CustomLabelProvider
    Signed-off-by: Christian Biasuzzi <christian.biasuzzi@soft.it>

--- network-area-diagram/src/main/java/com/powsybl/nad/svg/iidm/CustomLabelProvider.java
+++ network-area-diagram/src/main/java/com/powsybl/nad/svg/iidm/CustomLabelProvider.java
@@ -19,13 +19,25 @@ import java.util.*;
  */
 public class CustomLabelProvider extends DefaultLabelProvider {
     final Map<String, CustomBranchLabels> branchLabels;
+    final Map<String, CustomThreeWtLabels> threeWtLabels;
+    final Map<String, String> busDescriptions;
+    final Map<String, List<String>> vlDescriptions;
+    final Map<String, List<String>> vlDetails;
 
     public record CustomBranchLabels(String side1, String middle, String side2, EdgeInfo.Direction arrow1, EdgeInfo.Direction arrow2) {
     }
 
-    public CustomLabelProvider(Network network, SvgParameters svgParameters, Map<String, CustomBranchLabels> branchLabels) {
+    public record CustomThreeWtLabels(String side1, String side2, String side3, EdgeInfo.Direction arrow1, EdgeInfo.Direction arrow2, EdgeInfo.Direction arrow3) {
+    }
+
+    public CustomLabelProvider(Network network, SvgParameters svgParameters, Map<String, CustomBranchLabels> branchLabels, Map<String, CustomThreeWtLabels> threeWtLabels,
+                               Map<String, String> busDescriptions, Map<String, List<String>> vlDescriptions, Map<String, List<String>> vlDetails) {
         super(network, svgParameters);
         this.branchLabels = Objects.requireNonNull(branchLabels);
+        this.threeWtLabels = Objects.requireNonNull(threeWtLabels);
+        this.busDescriptions = Objects.requireNonNull(busDescriptions);
+        this.vlDescriptions = Objects.requireNonNull(vlDescriptions);
+        this.vlDetails = Objects.requireNonNull(vlDetails);
     }
 
     @Override
@@ -41,8 +53,48 @@ public class CustomLabelProvider extends DefaultLabelProvider {
     }
 
     @Override
+    public Optional<EdgeInfo> getEdgeInfo(Graph graph, ThreeWtEdge edge) {
+        CustomThreeWtLabels threeWtLabels1 = threeWtLabels.get(edge.getEquipmentId());
+        ThreeWtEdge.Side edgeSide = edge.getSide();
+        String labelSide = null;
+        EdgeInfo.Direction arrowDirection = null;
+        if (threeWtLabels1 != null) {
+            switch (edgeSide) {
+                case ONE -> {
+                    labelSide = threeWtLabels1.side1;
+                    arrowDirection = threeWtLabels1.arrow1;
+                }
+                case TWO -> {
+                    labelSide = threeWtLabels1.side2;
+                    arrowDirection = threeWtLabels1.arrow2;
+                }
+                case THREE -> {
+                    labelSide = threeWtLabels1.side3;
+                    arrowDirection = threeWtLabels1.arrow3;
+                }
+            }
+        }
+        return Optional.of(new EdgeInfo("Custom", arrowDirection, null, labelSide));
+    }
+
+    @Override
     public String getLabel(Edge edge) {
         CustomBranchLabels bl = branchLabels.get(edge.getEquipmentId());
         return (bl != null) ? bl.middle : null;
+    }
+
+    @Override
+    public String getBusDescription(BusNode busNode) {
+        return busDescriptions.get(busNode.getEquipmentId());
+    }
+
+    @Override
+    public List<String> getVoltageLevelDescription(VoltageLevelNode voltageLevelNode) {
+        return vlDescriptions.getOrDefault(voltageLevelNode.getEquipmentId(), Collections.emptyList());
+    }
+
+    @Override
+    public List<String> getVoltageLevelDetails(VoltageLevelNode vlNode) {
+        return vlDetails.getOrDefault(vlNode.getEquipmentId(), Collections.emptyList());
     }
 }
--- network-area-diagram/src/test/java/com/powsybl/nad/svg/CustomLabelProviderTest.java
+++ network-area-diagram/src/test/java/com/powsybl/nad/svg/CustomLabelProviderTest.java
@@ -9,14 +9,16 @@ package com.powsybl.nad.svg;
 
 import com.powsybl.diagram.test.Networks;
 import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.test.ThreeWindingsTransformerNetworkFactory;
 import com.powsybl.nad.AbstractTest;
 import com.powsybl.nad.layout.LayoutParameters;
 import com.powsybl.nad.svg.iidm.CustomLabelProvider;
-import com.powsybl.nad.svg.iidm.NominalVoltageStyleProvider;
+import com.powsybl.nad.svg.iidm.TopologicalStyleProvider;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -33,13 +35,13 @@ class CustomLabelProviderTest extends AbstractTest {
                 .setSvgWidthAndHeightAdded(true)
                 .setFixedWidth(800)
                 .setEdgeNameDisplayed(true)
-                .setVoltageLevelDetails(false)
-                .setBusLegend(false));
+                .setVoltageLevelDetails(true)
+                .setBusLegend(true));
     }
 
     @Override
     protected StyleProvider getStyleProvider(Network network) {
-        return new NominalVoltageStyleProvider(network);
+        return new TopologicalStyleProvider(network);
     }
 
     @Override
@@ -57,8 +59,50 @@ class CustomLabelProviderTest extends AbstractTest {
         branchLabels.put("L11", new CustomLabelProvider.CustomBranchLabels(null, "L2", null, EdgeInfo.Direction.IN, EdgeInfo.Direction.IN));
         branchLabels.put("T11", new CustomLabelProvider.CustomBranchLabels(null, "TWT2", "TWT2_2", null, EdgeInfo.Direction.OUT));
 
-        labelProvider = new CustomLabelProvider(network, getSvgParameters(), branchLabels);
+        Map<String, CustomLabelProvider.CustomThreeWtLabels> threeWtLabels = new HashMap<>();
+
+        Map<String, String> busDescriptions = new HashMap<>();
+        busDescriptions.put("VL1_10", "VL1 10");
+        busDescriptions.put("VL2_30", "VL2 30");
+
+        Map<String, List<String>> vlDescriptions = new HashMap<>();
+        vlDescriptions.put("VL1", List.of("VL1 description1", "VL1 description2"));
+        vlDescriptions.put("VL2", List.of("VL2 description1"));
+
+        Map<String, List<String>> vlDetails = new HashMap<>();
+        vlDetails.put("VL2", List.of("VL2 details1", "VL2 details2"));
+
+        labelProvider = new CustomLabelProvider(network, getSvgParameters(), branchLabels, threeWtLabels, busDescriptions, vlDescriptions, vlDetails);
 
         assertSvgEquals("/custom_label_provider.svg", network);
     }
+
+    @Test
+    void testCustomLabelProvider3wt() {
+        Network network = ThreeWindingsTransformerNetworkFactory.create();
+
+        Map<String, CustomLabelProvider.CustomBranchLabels> branchLabels = new HashMap<>();
+        Map<String, CustomLabelProvider.CustomThreeWtLabels> threeWtLabels = new HashMap<>();
+        threeWtLabels.put("3WT", new CustomLabelProvider.CustomThreeWtLabels("SIDE1", "SIDE2", "SIDE3", EdgeInfo.Direction.IN, EdgeInfo.Direction.OUT, EdgeInfo.Direction.IN));
+
+        Map<String, String> busDescriptions = new HashMap<>();
+        busDescriptions.put("VL_132_0", "VL1 132");
+        busDescriptions.put("VL_33_0", "VL2 33");
+        busDescriptions.put("VL_11_0", "VL2 11");
+
+        Map<String, List<String>> vlDescriptions = new HashMap<>();
+        vlDescriptions.put("VL_132", List.of("VL 132 description1", "VL1 132 description2"));
+        vlDescriptions.put("VL_33", List.of("VL 33 description1"));
+        vlDescriptions.put("VL_11", List.of("VL 11 description1"));
+
+        Map<String, List<String>> vlDetails = new HashMap<>();
+        vlDetails.put("VL_132", List.of("VL 132 details1"));
+        vlDetails.put("VL_33", List.of("VL 33 details1", "VL 33 details2"));
+        vlDetails.put("VL_11", List.of("VL 1 details1"));
+
+        labelProvider = new CustomLabelProvider(network, getSvgParameters(), branchLabels, threeWtLabels, busDescriptions, vlDescriptions, vlDetails);
+
+        assertSvgEquals("/custom_label_provider_3wt.svg", network);
+    }
+
 }
