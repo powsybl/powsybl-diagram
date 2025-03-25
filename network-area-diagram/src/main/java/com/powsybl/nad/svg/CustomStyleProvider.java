6901cdcb4ad5f9ecd89bf78eb509341ceb59efca
Christian Biasuzzi <christian.biasuzzi@soft.it>
2025-03-25 22:56:22 +0100

    adds a javadoc and removes redundant prefixes
    Signed-off-by: Christian Biasuzzi <christian.biasuzzi@soft.it>

--- network-area-diagram/src/main/java/com/powsybl/nad/svg/CustomStyleProvider.java
+++ network-area-diagram/src/main/java/com/powsybl/nad/svg/CustomStyleProvider.java
@@ -17,30 +17,54 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
+ * Enables the customization of the style of NAD elements: Bus nodes, branch-edges and three-winding-transformers edges.
+ *
+ * <p>
+ * NAD elements'style data is defined in the CustomStyleProvider constructor's map parameters.
+ *
+ * <p>
+ * The busNodesStyles map is indexed by the bus ID and defines the style for the bus nodes.
+ * In the map, a node style is declared in a BusNodeStyles record: fill, edge and edgeWidth are the fill color, the edge color and the edge size for the node, respectively.
+ *
+ * <p>
+ * The edgesStyles map is indexed by the branch ID and defines the style for the edges.
+ * In the map, the edge style is declared in a EdgeStyles record: edge1, width1 and dash1 are the color, the size and a dash pattern for the first half edge, respectively.
+ * Edge2, width2 and dash2 are the color, the size and a dash pattern for the second half edge.
+ *
+ * <p>
+ * The threeWtsStyles map is index by the three-winding-transformer ID and defines the style for the transformer’s legs.
+ * In the map, the style is declared in a ThreeWtStyles record: edge1, width1, dash1, edge2, width2 and dash2, edge3, width23 and dash3,
+ * are the color, the size and a dash pattern for the three legs of the transformer.
+ *
+ * <p>
+ * Note that the edge size is a string, it can be specified in pixel (e.g, 4px).
+ * A dash pattern is a string with a sequence of comma and/or white space separated lengths and percentages, that specify the lengths of alternating dashes and gaps in the edge.
+ * Elements that do not have a style specified in the parameters will be displayed with a default style.
+ *
  * @author Christian Biasuzzi {@literal <christian.biasuzzi at soft.it>}
  */
 public class CustomStyleProvider extends AbstractStyleProvider {
 
-    final Map<String, CustomBusNodeStyles> busNodesStyles;
-    final Map<String, CustomEdgeStyles> edgesStyles;
-    final Map<String, CustomThreeWtStyles> threeWtsStyles;
+    final Map<String, BusNodeStyles> busNodesStyles;
+    final Map<String, EdgeStyles> edgesStyles;
+    final Map<String, ThreeWtStyles> threeWtsStyles;
 
-    public record CustomBusNodeStyles(String fill, String edge, String edgeWidth) {
+    public record BusNodeStyles(String fill, String edge, String edgeWidth) {
     }
 
-    public record CustomEdgeStyles(String edge1, String width1, String dash1, String edge2, String width2,
-                                   String dash2) {
+    public record EdgeStyles(String edge1, String width1, String dash1, String edge2, String width2,
+                             String dash2) {
     }
 
-    public record CustomThreeWtStyles(String edge1, String width1, String dash1, String edge2, String width2,
-                                      String dash2, String edge3, String width3, String dash3) {
+    public record ThreeWtStyles(String edge1, String width1, String dash1, String edge2, String width2,
+                                String dash2, String edge3, String width3, String dash3) {
     }
 
-    private record CustomEdgeStyle(String stroke, String strokeWidth, String dash) {
+    private record EdgeStyle(String stroke, String strokeWidth, String dash) {
     }
 
-    public CustomStyleProvider(Map<String, CustomBusNodeStyles> busNodesStyles, Map<String, CustomEdgeStyles> edgesStyles,
-                               Map<String, CustomThreeWtStyles> threeWtsStyles) {
+    public CustomStyleProvider(Map<String, BusNodeStyles> busNodesStyles, Map<String, EdgeStyles> edgesStyles,
+                               Map<String, ThreeWtStyles> threeWtsStyles) {
         this.busNodesStyles = Objects.requireNonNull(busNodesStyles);
         this.edgesStyles = Objects.requireNonNull(edgesStyles);
         this.threeWtsStyles = Objects.requireNonNull(threeWtsStyles);
@@ -53,7 +77,7 @@ public class CustomStyleProvider extends AbstractStyleProvider {
 
     @Override
     public String getBusNodeStyle(BusNode busNode) {
-        CustomBusNodeStyles style = busNodesStyles.get(busNode.getEquipmentId());
+        BusNodeStyles style = busNodesStyles.get(busNode.getEquipmentId());
         if (style != null) {
             List<String> parts = new ArrayList<>();
             if (style.fill() != null) {
@@ -71,13 +95,13 @@ public class CustomStyleProvider extends AbstractStyleProvider {
         return null;
     }
 
-    private CustomEdgeStyle getEdgeStyle(CustomEdgeStyles styles, BranchEdge.Side side) {
+    private EdgeStyle getEdgeStyle(EdgeStyles styles, BranchEdge.Side side) {
         return (side == BranchEdge.Side.ONE)
-                ? new CustomEdgeStyle(styles.edge1(), styles.width1(), styles.dash1())
-                : new CustomEdgeStyle(styles.edge2(), styles.width2(), styles.dash2());
+                ? new EdgeStyle(styles.edge1(), styles.width1(), styles.dash1())
+                : new EdgeStyle(styles.edge2(), styles.width2(), styles.dash2());
     }
 
-    private String formatEdgeStyle(CustomEdgeStyle lineStyle) {
+    private String formatEdgeStyle(EdgeStyle lineStyle) {
         return Stream.of(
                         Optional.ofNullable(lineStyle.stroke()).map(stroke -> String.format("stroke:%s;", stroke)).orElse(null),
                         Optional.ofNullable(lineStyle.strokeWidth()).map(width -> String.format("stroke-width:%s;", width)).orElse(null),
@@ -87,11 +111,11 @@ public class CustomStyleProvider extends AbstractStyleProvider {
                 .collect(Collectors.joining(" "));
     }
 
-    private CustomEdgeStyle getThreeWtStyle(CustomThreeWtStyles styles, ThreeWtEdge.Side side) {
+    private EdgeStyle getThreeWtStyle(ThreeWtStyles styles, ThreeWtEdge.Side side) {
         return switch (side) {
-            case ONE -> new CustomEdgeStyle(styles.edge1(), styles.width1(), styles.dash1());
-            case TWO -> new CustomEdgeStyle(styles.edge2(), styles.width2(), styles.dash2());
-            case THREE -> new CustomEdgeStyle(styles.edge3(), styles.width3(), styles.dash3());
+            case ONE -> new EdgeStyle(styles.edge1(), styles.width1(), styles.dash1());
+            case TWO -> new EdgeStyle(styles.edge2(), styles.width2(), styles.dash2());
+            case THREE -> new EdgeStyle(styles.edge3(), styles.width3(), styles.dash3());
         };
     }
 
--- network-area-diagram/src/test/java/com/powsybl/nad/svg/CustomStyleProviderTest.java
+++ network-area-diagram/src/test/java/com/powsybl/nad/svg/CustomStyleProviderTest.java
@@ -11,9 +11,9 @@ import com.powsybl.diagram.test.Networks;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.nad.AbstractTest;
 import com.powsybl.nad.layout.LayoutParameters;
-import com.powsybl.nad.svg.CustomStyleProvider.CustomBusNodeStyles;
-import com.powsybl.nad.svg.CustomStyleProvider.CustomEdgeStyles;
-import com.powsybl.nad.svg.CustomStyleProvider.CustomThreeWtStyles;
+import com.powsybl.nad.svg.CustomStyleProvider.BusNodeStyles;
+import com.powsybl.nad.svg.CustomStyleProvider.EdgeStyles;
+import com.powsybl.nad.svg.CustomStyleProvider.ThreeWtStyles;
 import com.powsybl.nad.svg.iidm.DefaultLabelProvider;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -53,19 +53,19 @@ class CustomStyleProviderTest extends AbstractTest {
     void testCustomStyleProvider() {
         Network network = Networks.createNodeBreakerNetworkWithBranchStatus("TestNodeDecorators", "test");
 
-        Map<String, CustomBusNodeStyles> busNodesStyles = new HashMap<>();
-        busNodesStyles.put("VL1_10", new CustomBusNodeStyles("yellow", null, null));
-        busNodesStyles.put("VL2_30", new CustomBusNodeStyles("red", "black", "4px"));
+        Map<String, BusNodeStyles> busNodesStyles = new HashMap<>();
+        busNodesStyles.put("VL1_10", new BusNodeStyles("yellow", null, null));
+        busNodesStyles.put("VL2_30", new BusNodeStyles("red", "black", "4px"));
 
-        Map<String, CustomEdgeStyles> edgesStyles = new HashMap<>();
-        edgesStyles.put("L11", new CustomEdgeStyles("blue", "2px", null, "blue", "2px", null));
-        edgesStyles.put("L12", new CustomEdgeStyles("green", "4px", "8,4", "green", "4px", "8,4"));
-        edgesStyles.put("T11", new CustomEdgeStyles("red", "8px", null, "brown", "8px", null));
-        edgesStyles.put("T12", new CustomEdgeStyles("orange", null, null, "orange", null, null));
+        Map<String, EdgeStyles> edgesStyles = new HashMap<>();
+        edgesStyles.put("L11", new EdgeStyles("blue", "2px", null, "blue", "2px", null));
+        edgesStyles.put("L12", new EdgeStyles("green", "4px", "8,4", "green", "4px", "8,4"));
+        edgesStyles.put("T11", new EdgeStyles("red", "8px", null, "brown", "8px", null));
+        edgesStyles.put("T12", new EdgeStyles("orange", null, null, "orange", null, null));
 
-        Map<String, CustomThreeWtStyles> threeWtsStyles = new HashMap<>();
+        Map<String, ThreeWtStyles> threeWtsStyles = new HashMap<>();
         threeWtsStyles.put("T3_12",
-                new CustomThreeWtStyles(
+                new ThreeWtStyles(
                         "gray", "4px", null,
                         "purple", "4px", "4,4",
                         "pink", "6px", null
