1f8d980e54a07f0b2a94e0f61d42ce897142da5a
NathanDissoubray <nathan.dissoubray@rte-france.com>
2025-05-19 16:04:23 +0200

    Add an explanation on the calculations of CoulombForce
    Signed-off-by: NathanDissoubray <nathan.dissoubray@rte-france.com>

--- diagram-util/src/main/java/com/powsybl/diagram/util/forcelayout/forces/CoulombForce.java
+++ diagram-util/src/main/java/com/powsybl/diagram/util/forcelayout/forces/CoulombForce.java
@@ -44,7 +44,15 @@ public class CoulombForce<V, E> extends AbstractForce<V, E> {
     private void coulombBetweenPoints(Vector2D resultingForce, Point correspondingPoint, Point otherPoint) {
         Vector2D force = Vector2D.calculateVectorBetweenPoints(otherPoint, correspondingPoint);
         double magnitude = force.magnitude();
-        // 0.5 because we assume both points are moving, so each does half of the movement
+        /// this is the contracted version of calculating Coulomb, if we take V as the vector from otherPoint to correspondingPoint, then
+        /// if we take M(V) the magnitude of the vector V:
+        /// F = V / M(V) * forceIntensity / M(V)^2
+        /// The original version of the code did F = V / M(V) * forceIntensity / (M(V)^2 * 0.5 + 0.1)
+        /// adding 0.1 was to prevent division by 0 (which could still happen when normalizing the vector
+        /// multiplying by 0.5 is supposed to be used to tell that each point is moved by half of the force (since both points move)
+        /// but that was incorrect since we multiply by 0.5 at the denominator, meaning we multiply by 2 the force
+        /// In a goal of getting the same results after the refactor, this implementation of Coulomb does the same
+        /// even if it could be considered "incorrect" compared to the original goal of those operations
         double intensity = forceParameter.getForceIntensity() / (magnitude * magnitude * magnitude * 0.5 + 0.1 * magnitude);
         force.multiplyBy(intensity);
         resultingForce.add(force);
