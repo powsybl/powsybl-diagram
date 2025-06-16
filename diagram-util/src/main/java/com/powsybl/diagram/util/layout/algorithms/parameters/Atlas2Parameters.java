fd495de7b14fe03a326782ad53c061c3caa5f807
NathanDissoubray <nathan.dissoubray@rte-france.com>
2025-06-16 16:39:29 +0200

    Use quadtree for barnes-hut in atlas2
    Signed-off-by: NathanDissoubray <nathan.dissoubray@rte-france.com>

--- diagram-util/src/main/java/com/powsybl/diagram/util/layout/algorithms/parameters/Atlas2Parameters.java
+++ diagram-util/src/main/java/com/powsybl/diagram/util/layout/algorithms/parameters/Atlas2Parameters.java
@@ -217,8 +217,9 @@ public final class Atlas2Parameters {
                 throw new IllegalArgumentException("iterationNumberIncreasePercent should be strictly positive, as the number of iterations cannot be reduced to less than the stopping criterion");
             }
             this.iterationNumberIncreasePercent = iterationNumberIncreasePercent;
-	    return this;
-	}
+            return this;
+        }
+
         public Builder withBarnesHutTheta(double barnesHutTheta) {
             if (barnesHutTheta < 0) {
                 throw new IllegalArgumentException("The theta of the Barnes Hut optimization cannot be a negative value");
@@ -261,7 +262,7 @@ public final class Atlas2Parameters {
         /// You can do so by using withBarnesHutTheta(0)
         /// This might be interesting to do especially if you need to run Atlas on a lot of small networks
         public Atlas2Parameters build() {
-            return new Atlas2Parameters<>(
+            return new Atlas2Parameters(
                     maxSteps,
                     repulsion,
                     edgeAttraction,
