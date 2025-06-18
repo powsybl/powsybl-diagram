306f2511e7a6200fe7cb144b46b294a8283923fd
NathanDissoubray <nathan.dissoubray@rte-france.com>
2025-06-18 11:37:32 +0200

    Change name of function in tests
    Signed-off-by: NathanDissoubray <nathan.dissoubray@rte-france.com>

--- diagram-util/src/test/java/com/powsybl/diagram/util/layout/algorithms/Atlas2ForceLayoutAlgorithmTest.java
+++ diagram-util/src/test/java/com/powsybl/diagram/util/layout/algorithms/Atlas2ForceLayoutAlgorithmTest.java
@@ -67,7 +67,7 @@ class Atlas2ForceLayoutAlgorithmTest {
 
     @Test
     void calculateLayoutYesBH() {
-        ForceGraph<String, DefaultEdge> forceGraph = GraphTestData.getForcegraph();
+        ForceGraph<String, DefaultEdge> forceGraph = GraphTestData.getForcegraph1();
         Atlas2Parameters<String, DefaultEdge> layoutParameters = new Atlas2Parameters.Builder().withBarnesHutTheta(1.5).build();
         LayoutAlgorithm<String, DefaultEdge> atlas2 = new Atlas2Layout<>(layoutParameters);
         atlas2.run(layoutContext);
