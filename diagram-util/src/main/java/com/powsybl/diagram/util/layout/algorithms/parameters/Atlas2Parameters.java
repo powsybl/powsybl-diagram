0b6a8c6a64b6d228f67d2511feccf870f38dad71
NathanDissoubray <nathan.dissoubray@rte-france.com>
2025-06-24 16:30:28 +0200

    Add a note about deactivating Barnes-Hut for small networks
    Signed-off-by: NathanDissoubray <nathan.dissoubray@rte-france.com>

--- diagram-util/src/main/java/com/powsybl/diagram/util/layout/algorithms/parameters/Atlas2Parameters.java
+++ diagram-util/src/main/java/com/powsybl/diagram/util/layout/algorithms/parameters/Atlas2Parameters.java
@@ -220,6 +220,11 @@ public final class Atlas2Parameters {
             return this;
         }
 
+        /// By default, the Barnes-Hut optimisation is activated with theta = 1.2
+        /// If your network has a small number of nodes (less than 500), Atlas might finish faster by deactivating Barnes-Hut
+        /// You can do so by using withBarnesHutTheta(0)
+        /// This might be interesting to do especially if you need to run Atlas on a lot of small networks
+	
         public Atlas2Parameters build() {
             return new Atlas2Parameters<>(
                     maxSteps,
