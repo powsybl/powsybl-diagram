8523f7b49a9da196d763e3a6bee3e1618d7cb047
NathanDissoubray <nathan.dissoubray@rte-france.com>
2025-07-22 15:54:35 +0200

    typo
    Signed-off-by: NathanDissoubray <nathan.dissoubray@rte-france.com>

--- diagram-util/src/main/java/com/powsybl/diagram/util/forcelayout/setup/Setup.java
+++ diagram-util/src/main/java/com/powsybl/diagram/util/forcelayout/setup/Setup.java
@@ -17,7 +17,7 @@ public interface Setup<V, E> {
      * Set up the layout context so the layout algorithm can run on it properly<br>
      * An implementation of a setup is expected to do at least the following:<br>
      * - fill the <code>fixedPoints</code> and <code>movingPoints</code> Map of <code>layoutContext</code> depending on if the vertex are in <code>fixedNodes</code> or not<br>
-     * - use <code>initialPoints</code> to give a starting position to all the points, or create a position if not initial position is given<br>
+     * - use <code>initialPoints</code> to give a starting position to all the points, or create a position if no initial position is given<br>
      * Regarding the initial position, take care that the created positions do not overlap too much. Some forces such as repulsion forces will not work if points are at the exact same place<br>
      * It is therefore ill-advised to put all the points on (0, 0) for example.
      * For a simple example, see {@link com.powsybl.diagram.util.forcelayout.setup.SimpleSetup}
