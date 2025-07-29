ab79759140a8c30a21797b97e894acb7e17748c7
NathanDissoubray <nathan.dissoubray@rte-france.com>
2025-07-29 14:07:51 +0200

    Add more tests
    Signed-off-by: NathanDissoubray <nathan.dissoubray@rte-france.com>

--- network-area-diagram/src/test/java/com/powsybl/nad/layout/Atlas2ForceLayoutTest.java
+++ network-area-diagram/src/test/java/com/powsybl/nad/layout/Atlas2ForceLayoutTest.java
@@ -41,4 +41,45 @@ class Atlas2ForceLayoutTest extends ForceLayoutTest {
         assertSvgEquals("/IEEE_14_atlas2_custom1.svg", IeeeCdfNetworkFactory.create14(), VoltageLevelFilter.NO_FILTER, atlas2ForceLayout);
     }
 
+    @Test
+    void testIEEE14CustomMoreIteration() {
+        Atlas2ForceLayout atlas2ForceLayout = new Atlas2ForceLayout(
+                new SquareRandomSetup<>(),
+                new Atlas2Parameters.Builder()
+                        .withRepulsion(10)
+                        .withAttractToCenterForce(false)
+                        .withSwingTolerance(0.8)
+                        .withIterationNumberIncreasePercent(0.5)
+                        .build()
+        );
+        assertSvgEquals("/IEEE_14_atlas2_custom1_moreIteration.svg", IeeeCdfNetworkFactory.create14(), VoltageLevelFilter.NO_FILTER, atlas2ForceLayout);
+    }
+
+    @Test
+    void testIEEE30Custom() {
+        Atlas2ForceLayout atlas2ForceLayout = new Atlas2ForceLayout(
+                new SquareRandomSetup<>(),
+                new Atlas2Parameters.Builder()
+                        .withGravity(0.003)
+                        .withMaxSpeedFactor(8)
+                        .withSpeedFactor(0.9)
+                        .withMaxSteps(200)
+                        .withIterationNumberIncreasePercent(0.6)
+                        .build()
+        );
+        assertSvgEquals("/IEEE_30_atlas2_custom1.svg", IeeeCdfNetworkFactory.create30(), VoltageLevelFilter.NO_FILTER, atlas2ForceLayout);
+    }
+
+    @Test
+    void testIEEE118Custom() {
+        Atlas2ForceLayout atlas2ForceLayout = new Atlas2ForceLayout(
+                new SquareRandomSetup<>(),
+                new Atlas2Parameters.Builder()
+                        .withRepulsion(10)
+                        .withIterationNumberIncreasePercent(0.2)
+                        .build()
+        );
+        assertSvgEquals("/IEEE_118_atlas2_custom1.svg", IeeeCdfNetworkFactory.create118(), VoltageLevelFilter.NO_FILTER, atlas2ForceLayout);
+    }
+
 }
