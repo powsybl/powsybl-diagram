629308e3375132979b7a6a0c58f21b7c3692e15c
NathanDissoubray <nathan.dissoubray@rte-france.com>
2025-07-29 16:46:05 +0200

    Add doc for the parameters of BasicForceLayout
    Signed-off-by: NathanDissoubray <nathan.dissoubray@rte-france.com>

--- diagram-util/src/main/java/com/powsybl/diagram/util/layout/algorithms/parameters/BasicForceLayoutParameters.java
+++ diagram-util/src/main/java/com/powsybl/diagram/util/layout/algorithms/parameters/BasicForceLayoutParameters.java
@@ -18,6 +18,8 @@ public final class BasicForceLayoutParameters {
     private static final double DEFAULT_REPULSION = 800.0;
     private static final double DEFAULT_FRICTION = 500;
     private static final double DEFAULT_MAX_SPEED = 100;
+    private static final boolean DEFAULT_REPULSION_FROM_FIXED_POINTS = true;
+    private static final boolean DEFAULT_ATTRACT_TO_CENTER_FORCES = true;
 
     private final int maxSteps;
     private final double minEnergyThreshold;
@@ -55,44 +57,93 @@ public final class BasicForceLayoutParameters {
         private double repulsion = DEFAULT_REPULSION;
         private double friction = DEFAULT_FRICTION;
         private double maxSpeed = DEFAULT_MAX_SPEED;
-        private boolean repulsionForceFromFixedPoints = true;
-        private boolean attractToCenterForce = true;
+        private boolean repulsionForceFromFixedPoints = DEFAULT_REPULSION_FROM_FIXED_POINTS;
+        private boolean attractToCenterForce = DEFAULT_ATTRACT_TO_CENTER_FORCES;
 
+        /**
+         * Change the maximum number of iteration the algorithm is allowed to run,
+         * default is {@value DEFAULT_MAX_STEPS}
+         * @param maxSteps the maximum number of iteration
+         * @return the instance of this Builder with the `maxSteps` changed
+         */
         public Builder withMaxSteps(int maxSteps) {
             this.maxSteps = maxSteps;
             return this;
         }
 
+        /**
+         * Control the energy that each point has to reach before stopping the algorithm.
+         * The lower the value, the longer the run, but it might provide better results visually,
+         * default is {@value DEFAULT_MIN_ENERGY_THRESHOLD}
+         * @param minEnergyThreshold the value of the energy
+         * @return the instance of this Builder with the `minEnergyThreshold` changed
+         */
         public Builder withMinEnergyThreshold(double minEnergyThreshold) {
             this.minEnergyThreshold = minEnergyThreshold;
             return this;
         }
 
+        /**
+         * The time interval between each simulation step. A lower number means the force is more precise, but that runtime might be longer,
+         * default is {@value DEFAULT_DELTA_TIME}
+         * @param deltaTime the time between two steps of the force graph simulation
+         * @return the instance of this Builder with the `deltaTime` changed
+         */
         public Builder withDeltaTime(double deltaTime) {
             this.deltaTime = deltaTime;
             return this;
         }
 
+        /**
+         * Coefficient for the repulsion force between nodes. Increasing this will make the network more sparse (ie nodes will be further apart),
+         * default is {@value DEFAULT_REPULSION}
+         * @param repulsion the repulsion coefficient you want
+         * @return the instance of this Builder with the `repulsion` changed
+         */
         public Builder withRepulsion(double repulsion) {
             this.repulsion = repulsion;
             return this;
         }
 
+        /**
+         * Coefficient for the slowdown of the points, the higher the coefficient, the slower a point will be,
+         * default is {@value DEFAULT_FRICTION}
+         * @param friction the value of the friction
+         * @return the instance of this Builder with the `friction` changed
+         */
         public Builder withFriction(double friction) {
             this.friction = friction;
             return this;
         }
 
+        /**
+         * The maximum speed a point can have, higher values might allow faster convergence, but if set too high could start having the opposite effect,
+         * with points starting to swing around their equilibrium position, default is {@value DEFAULT_MAX_SPEED}
+         * @param maxSpeed the maximum speed of a point
+         * @return the instance of this Builder with the `maxSpeed` changed
+         */
         public Builder withMaxSpeed(double maxSpeed) {
             this.maxSpeed = maxSpeed;
             return this;
         }
 
+        /**
+         * If set to true, other points will get a repulsion effect from unmovable points (fixed points),
+         * default is {@value DEFAULT_REPULSION_FROM_FIXED_POINTS}
+         * @param repulsionForceFromFixedPoints whether you want to activate repulsion from fixed points or not
+         * @return the instance of this Builder with the `repulsionForceFromFixedPoints` changed
+         */
         public Builder withRepulsionForceFromFixedPoints(boolean repulsionForceFromFixedPoints) {
             this.repulsionForceFromFixedPoints = repulsionForceFromFixedPoints;
             return this;
         }
 
+        /**
+         * Activate or deactivate the force that attracts points to the center of the graph. It is used to prevent non-connected points
+         * from drifting away, default is {@value DEFAULT_ATTRACT_TO_CENTER_FORCES}
+         * @param attractToCenterForce activate or deactivate the center attraction force
+         * @return the instance of this Builder with the `attractToCenterForce` changed
+         */
         public Builder withAttractToCenterForce(boolean attractToCenterForce) {
             this.attractToCenterForce = attractToCenterForce;
             return this;
