43faf60b9367b6e7fb0e37b594a7fb2cd638cc09
Caroline Jeandat <caroline.jeandat@rte-france.com>
2025-12-04 13:57:16 +0100

    add test for three windings transformer
    Signed-off-by: Caroline Jeandat <caroline.jeandat@rte-france.com>

--- single-line-diagram/single-line-diagram-core/src/test/java/com/powsybl/sld/iidm/Test3WTFeederInfoArrow.java
+++ single-line-diagram/single-line-diagram-core/src/test/java/com/powsybl/sld/iidm/Test3WTFeederInfoArrow.java
@@ -11,6 +11,8 @@ import com.powsybl.iidm.network.test.ThreeWindingsTransformerNetworkFactory;
 import com.powsybl.sld.builders.NetworkGraphBuilder;
 import com.powsybl.sld.model.graphs.SubstationGraph;
 import com.powsybl.sld.model.graphs.VoltageLevelGraph;
+import com.powsybl.sld.svg.DefaultLabelProvider;
+import com.powsybl.sld.svg.styles.BasicStyleProvider;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -38,6 +40,16 @@ class Test3WTFeederInfoArrow extends AbstractTestCaseIidm {
         voltageLevelGraphLayout(g);
 
         assertEquals(toString("/Test3WTFeederInfoArrowVoltageLevel.svg"), toSVG(g, "/Test3WTFeederInfoArrowVoltageLevel.svg"));
+
+        // Test with all possible feeder infos
+        layoutParameters.setSpaceForFeederInfos(90);
+        DefaultLabelProvider labelProvider = getDefaultDiagramLabelProvider();
+        labelProvider.setDisplayCurrent(true);
+        labelProvider.setDisplayArrowForCurrent(true);
+        labelProvider.setDisplayPermanentLimitPercentage(true);
+
+        assertEquals(toString("/Test3WTFeederInfoArrowVoltageLevelWithAllPossibleFeederInfos.svg"), toSVG(g, "/Test3WTFeederInfoArrowVoltageLevelWithAllPossibleFeederInfos.svg", componentLibrary, layoutParameters, svgParameters, labelProvider, new BasicStyleProvider()));
+
     }
 
     @Test
