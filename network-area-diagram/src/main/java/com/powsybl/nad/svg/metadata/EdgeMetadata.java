ab8bcdd61a40175d80796459bb9e984903518bbf
Florian Dupuy <66690739+flo-dup@users.noreply.github.com>
2025-12-09 15:13:34 +0100

    [NAD] Metadata add edge/node invisible booleans (#765)
    * Add invisible informations both on edges and nodes
    * Update unit test references
    
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- network-area-diagram/src/main/java/com/powsybl/nad/svg/metadata/EdgeMetadata.java
+++ network-area-diagram/src/main/java/com/powsybl/nad/svg/metadata/EdgeMetadata.java
@@ -21,6 +21,8 @@ public class EdgeMetadata extends AbstractMetadataItem {
     private final String busNode1SvgId;
     private final String busNode2SvgId;
     private final String edgeType;
+    private final boolean invisibleSide1;
+    private final boolean invisibleSide2;
     private final EdgeInfoMetadata edgeInfo1;
     private final EdgeInfoMetadata edgeInfo2;
     private final EdgeInfoMetadata edgeInfoMiddle;
@@ -32,6 +34,8 @@ public class EdgeMetadata extends AbstractMetadataItem {
                         @JsonProperty("busNode1") String busNode1SvgId,
                         @JsonProperty("busNode2") String busNode2SvgId,
                         @JsonProperty("type") String edgeType,
+                        @JsonProperty("invisible1") boolean invisibleSide1,
+                        @JsonProperty("invisible2") boolean invisibleSide2,
                         @JsonProperty("edgeInfo1") EdgeInfoMetadata edgeInfo1,
                         @JsonProperty("edgeInfo2") EdgeInfoMetadata edgeInfo2,
                         @JsonProperty("edgeInfoMiddle") EdgeInfoMetadata edgeInfoMiddle) {
@@ -41,6 +45,8 @@ public class EdgeMetadata extends AbstractMetadataItem {
         this.busNode1SvgId = busNode1SvgId;
         this.busNode2SvgId = busNode2SvgId;
         this.edgeType = edgeType;
+        this.invisibleSide1 = invisibleSide1;
+        this.invisibleSide2 = invisibleSide2;
         this.edgeInfo1 = edgeInfo1;
         this.edgeInfo2 = edgeInfo2;
         this.edgeInfoMiddle = edgeInfoMiddle;
@@ -69,6 +75,18 @@ public class EdgeMetadata extends AbstractMetadataItem {
     @JsonProperty("type")
     public String getEdgeType() {
         return edgeType;
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    @JsonProperty("invisible1")
+    public boolean isInvisibleSide1() {
+        return invisibleSide1;
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+    @JsonProperty("invisible2")
+    public boolean isInvisibleSide2() {
+        return invisibleSide2;
     }
 
     @JsonProperty("edgeInfo1")
