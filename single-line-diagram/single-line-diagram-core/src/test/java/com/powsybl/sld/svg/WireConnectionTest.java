5d7cde8bff52c24fda868665fb5c3d63f4d57778
Florian Dupuy <florian.dupuy@rte-france.com>
2026-03-09 13:21:08 +0100

    WireConnectionTest: use templates and fix checkstyle
    Signed-off-by: Florian Dupuy <florian.dupuy@rte-france.com>

--- single-line-diagram/single-line-diagram-core/src/test/java/com/powsybl/sld/svg/WireConnectionTest.java
+++ single-line-diagram/single-line-diagram-core/src/test/java/com/powsybl/sld/svg/WireConnectionTest.java
@@ -12,9 +12,12 @@ import com.powsybl.sld.library.AnchorPoint;
 import com.powsybl.sld.model.coordinate.Point;
 import com.powsybl.sld.model.nodes.ConnectivityNode;
 import com.powsybl.sld.model.nodes.Node;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,10 +32,11 @@ class WireConnectionTest {
         return node;
     }
 
-    @Test
-    void testCalculatePolylinePoints_VerticalVertical() {
-        AnchorPoint apA = new AnchorPoint(0, 0, AnchorOrientation.VERTICAL);
-        AnchorPoint apB = new AnchorPoint(0, 0, AnchorOrientation.VERTICAL);
+    @ParameterizedTest
+    @MethodSource("calculatePolylinePointsProvider")
+    void testCalculatePolylinePoints(AnchorOrientation orientationA, AnchorOrientation orientationB, List<Double> expectedPoints) {
+        AnchorPoint apA = new AnchorPoint(0, 0, orientationA);
+        AnchorPoint apB = new AnchorPoint(0, 0, orientationB);
 
         Node nodeA = createConnectivityNode(10, 10);
         Node nodeB = createConnectivityNode(50, 50);
@@ -40,76 +44,17 @@ class WireConnectionTest {
         WireConnection wc = new WireConnection(apA, apB);
         List<Point> pol = wc.calculatePolylinePoints(nodeA, nodeB, false, new Point(0, 0));
 
-        assertEquals(List.of(10.0, 10.0, 50.0, 30.0, 10.0, 30.0, 50.0, 50.0), Point.pointsToDoubles(pol));
+        assertEquals(expectedPoints, Point.pointsToDoubles(pol));
     }
 
-    @Test
-    void testCalculatePolylinePoints_VerticalHorizontal() {
-        AnchorPoint apA = new AnchorPoint(0, 0, AnchorOrientation.VERTICAL);
-        AnchorPoint apB = new AnchorPoint(0, 0, AnchorOrientation.HORIZONTAL);
-
-        Node nodeA = createConnectivityNode(10, 10);
-        Node nodeB = createConnectivityNode(50, 50);
-
-        WireConnection wc = new WireConnection(apA, apB);
-        List<Point> pol = wc.calculatePolylinePoints(nodeA, nodeB, false, new Point(0, 0));
-
-        assertEquals(List.of(10.0, 10.0, 10.0, 50.0, 50.0, 50.0), Point.pointsToDoubles(pol));
-    }
-
-    @Test
-    void testCalculatePolylinePoints_HorizontalHorizontal() {
-        AnchorPoint apA = new AnchorPoint(0, 0, AnchorOrientation.HORIZONTAL);
-        AnchorPoint apB = new AnchorPoint(0, 0, AnchorOrientation.HORIZONTAL);
-
-        Node nodeA = createConnectivityNode(10, 10);
-        Node nodeB = createConnectivityNode(50, 50);
-
-        WireConnection wc = new WireConnection(apA, apB);
-        List<Point> pol = wc.calculatePolylinePoints(nodeA, nodeB, false, new Point(0, 0));
-
-        assertEquals(List.of(10.0, 10.0, 30.0, 50.0, 30.0, 10.0, 50.0, 50.0), Point.pointsToDoubles(pol));
-    }
-
-    @Test
-    void testCalculatePolylinePoints_HorizontalVertical() {
-        AnchorPoint apA = new AnchorPoint(0, 0, AnchorOrientation.HORIZONTAL);
-        AnchorPoint apB = new AnchorPoint(0, 0, AnchorOrientation.VERTICAL);
-
-        Node nodeA = createConnectivityNode(10, 10);
-        Node nodeB = createConnectivityNode(50, 50);
-
-        WireConnection wc = new WireConnection(apA, apB);
-        List<Point> pol = wc.calculatePolylinePoints(nodeA, nodeB, false, new Point(0, 0));
-
-        assertEquals(List.of(10.0, 10.0, 50.0, 10.0, 50.0, 50.0), Point.pointsToDoubles(pol));
-    }
-
-    @Test
-    void testCalculatePolylinePoints_NoneHorizontal() {
-        AnchorPoint apA = new AnchorPoint(0, 0, AnchorOrientation.NONE);
-        AnchorPoint apB = new AnchorPoint(0, 0, AnchorOrientation.HORIZONTAL);
-
-        Node nodeA = createConnectivityNode(10, 10);
-        Node nodeB = createConnectivityNode(50, 50);
-
-        WireConnection wc = new WireConnection(apA, apB);
-        List<Point> pol = wc.calculatePolylinePoints(nodeA, nodeB, false, new Point(0, 0));
-
-        assertEquals(List.of(10.0, 10.0, 10.0, 50.0, 50.0, 50.0), Point.pointsToDoubles(pol));
-    }
-
-    @Test
-    void testCalculatePolylinePoints_NoneVertical() {
-        AnchorPoint apA = new AnchorPoint(0, 0, AnchorOrientation.NONE);
-        AnchorPoint apB = new AnchorPoint(0, 0, AnchorOrientation.VERTICAL);
-
-        Node nodeA = createConnectivityNode(10, 10);
-        Node nodeB = createConnectivityNode(50, 50);
-
-        WireConnection wc = new WireConnection(apA, apB);
-        List<Point> pol = wc.calculatePolylinePoints(nodeA, nodeB, false, new Point(0, 0));
-
-        assertEquals(List.of(10.0, 10.0, 50.0, 10.0, 50.0, 50.0), Point.pointsToDoubles(pol));
+    private static Stream<Arguments> calculatePolylinePointsProvider() {
+        return Stream.of(
+            Arguments.of(AnchorOrientation.VERTICAL, AnchorOrientation.VERTICAL, List.of(10.0, 10.0, 50.0, 30.0, 10.0, 30.0, 50.0, 50.0)),
+            Arguments.of(AnchorOrientation.VERTICAL, AnchorOrientation.HORIZONTAL, List.of(10.0, 10.0, 10.0, 50.0, 50.0, 50.0)),
+            Arguments.of(AnchorOrientation.HORIZONTAL, AnchorOrientation.HORIZONTAL, List.of(10.0, 10.0, 30.0, 50.0, 30.0, 10.0, 50.0, 50.0)),
+            Arguments.of(AnchorOrientation.HORIZONTAL, AnchorOrientation.VERTICAL, List.of(10.0, 10.0, 50.0, 10.0, 50.0, 50.0)),
+            Arguments.of(AnchorOrientation.NONE, AnchorOrientation.HORIZONTAL, List.of(10.0, 10.0, 10.0, 50.0, 50.0, 50.0)),
+            Arguments.of(AnchorOrientation.NONE, AnchorOrientation.VERTICAL, List.of(10.0, 10.0, 50.0, 10.0, 50.0, 50.0))
+        );
     }
 }
