384b57ffcde381a753533a3f614ffa8e18c00cb8
NathanDissoubray <nathan.dissoubray@rte-france.com>
2026-04-21 15:33:35 +0200

    Add test
    Signed-off-by: NathanDissoubray <nathan.dissoubray@rte-france.com>

--- diagram-util/src/test/java/com/powsybl/diagram/util/layout/algorithms/Atlas2ForceLayoutAlgorithmTest.java
+++ diagram-util/src/test/java/com/powsybl/diagram/util/layout/algorithms/Atlas2ForceLayoutAlgorithmTest.java
@@ -12,12 +12,16 @@ import com.powsybl.diagram.util.layout.Layout;
 import com.powsybl.diagram.util.layout.ResourceUtils;
 import com.powsybl.diagram.util.layout.algorithms.parameters.Atlas2Parameters;
 import com.powsybl.diagram.util.layout.geometry.LayoutContext;
+import com.powsybl.diagram.util.layout.geometry.Point;
 import com.powsybl.diagram.util.layout.postprocessing.OverlapPreventionPostProcessing;
+import com.powsybl.diagram.util.layout.postprocessing.PostProcessing;
 import com.powsybl.diagram.util.layout.setup.SquareRandomSetup;
 import org.jgrapht.graph.DefaultEdge;
 import org.junit.jupiter.api.Test;
 
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -63,5 +67,29 @@ class Atlas2ForceLayoutAlgorithmTest {
         StringWriter sw = new StringWriter();
         layoutContext.toSVG(v -> String.format("Vertex %s", v), sw);
         assertEquals(ResourceUtils.toString("atlas2_5_nodes_yes_BH.svg"), sw.toString());
+    }
+
+    @Test
+    void calculateLayoutWithOverlappingPoints() {
+        LayoutContext<String, DefaultEdge> layoutContext = GraphTestData.getLayoutContext1();
+        //set position of 0 to be position of 1
+        layoutContext.getMovingPoints().get("0").setPosition(GraphTestData.getPoints1()[1].getPosition());
+        LayoutAlgorithm<String, DefaultEdge> atlas2 = new Atlas2ForceLayoutAlgorithm<>();
+        atlas2.run(layoutContext);
+        checkPointPositionAllDifferent(layoutContext);
+
+        layoutContext.getMovingPoints().get("2").setPosition(GraphTestData.getPoints1()[3].getPosition());
+        PostProcessing<String, DefaultEdge> noOverlapPostProcessing = new OverlapPreventionPostProcessing<>();
+        noOverlapPostProcessing.run(layoutContext);
+        checkPointPositionAllDifferent(layoutContext);
+    }
+
+    public void checkPointPositionAllDifferent(LayoutContext<String, DefaultEdge> layoutContext) {
+        List<Point> allPoints = new ArrayList<>(layoutContext.getAllPoints().values());
+        for (int i = 0; i < allPoints.size(); ++i) {
+            for (int j = i + 1; j < allPoints.size(); ++j) {
+                assertNotEquals(allPoints.get(i).getPosition(), allPoints.get(j).getPosition());
+            }
+        }
     }
 }
