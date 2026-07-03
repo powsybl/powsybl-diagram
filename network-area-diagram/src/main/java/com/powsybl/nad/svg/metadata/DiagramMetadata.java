74c9676aa59407ac09e30d1185a78cfc5f5c37f8
Christian Biasuzzi <christian.biasuzzi@soft.it>
2026-07-03 17:49:51 +0200

    adds classes attribute to the edges metadata (#824)
    Signed-off-by: Christian Biasuzzi <christian.biasuzzi@soft.it>

--- network-area-diagram/src/main/java/com/powsybl/nad/svg/metadata/DiagramMetadata.java
+++ network-area-diagram/src/main/java/com/powsybl/nad/svg/metadata/DiagramMetadata.java
@@ -157,7 +157,8 @@ public class DiagramMetadata extends AbstractMetadata {
                 edge.getEdgeStyleInfo(BranchEdge.Side.ONE).styleClasses(),
                 edge.getEdgeStyleInfo(BranchEdge.Side.TWO).styleClasses(),
                 edge.getEdgeStyleInfo(BranchEdge.Side.ONE).style(),
-                edge.getEdgeStyleInfo(BranchEdge.Side.TWO).style()
+                edge.getEdgeStyleInfo(BranchEdge.Side.TWO).style(),
+                edge.getStyleClasses()
         )));
         graph.getThreeWtEdgesStream().forEach(edge -> {
             String threeWtNodeSvgId = graph.getThreeWtNode(edge).getSvgId();
@@ -178,6 +179,7 @@ public class DiagramMetadata extends AbstractMetadata {
                     edge.getEdgeStyleInfo().styleClasses(),
                     null,
                     edge.getEdgeStyleInfo().style(),
+                    null,
                     null
             ));
         });
