4a26b0edce00fc74300e0df5d8b2e41a3c5c7a30
Samir Romdhani <samir.romdhani_externe@rte-france.com>
2026-09-03 14:55:02 +0200

    review
    Signed-off-by: Samir Romdhani <samir.romdhani_externe@rte-france.com>

--- single-line-diagram/single-line-diagram-core/src/test/java/com/powsybl/sld/LayoutIdempotencyTest.java
+++ single-line-diagram/single-line-diagram-core/src/test/java/com/powsybl/sld/LayoutIdempotencyTest.java
@@ -41,8 +41,8 @@ class LayoutIdempotencyTest extends AbstractTestCaseIidm {
         graphBuilder = new NetworkGraphBuilder(network);
         substation = Networks.createSubstation(network, "s", "s", Country.FR);
         vl = Networks.createVoltageLevel(substation, "vl", "vl", TopologyKind.NODE_BREAKER, 380);
-        Networks.createBusBarSection(vl, "bbs", "bbs", 0, 1, 1);
-        Networks.createLoad(vl, "l", "l", "l", 0, ConnectablePosition.Direction.TOP, 2, 10, 10);
+        Networks.createBusBarSection(vl, "bbs", 0, 1, 1);
+        Networks.createLoad(vl, "l", 0, ConnectablePosition.Direction.TOP, 2, 10, 10);
         Networks.createSwitch(vl, "d", "d", SwitchKind.DISCONNECTOR, false, false, false, 0, 1);
         Networks.createSwitch(vl, "b", "b", SwitchKind.BREAKER, false, false, false, 1, 2);
     }
